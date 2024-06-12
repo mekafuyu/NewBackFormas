8deb8c2c2204ead43c92b945845991f5d6648008
mekafuyu <mayconbrunob@hotmail.com>
2024-06-12 09:49:55 -0300

    anabelly mods

--- public/js/SearchBtn.js
+++ public/js/SearchBtn.js
@@ -1,15 +1,24 @@
 function filterBySearch(){
     const inputSearch = document.getElementById('searchInput');
     const filter = inputSearch.value.toLowerCase();
-    const cardprocess = document.getElementById('processCard');
+    const cardprocess = document.getElementsByClassName('card');
 
-    for (let i = 0; i < cardprocess.clientHeight; i++){
+    for (let i = 0; i < cardprocess.length; i++){
         const card = cardprocess[i];
         const cardContent = card.textContent || card.innerText;
+        const words = cardContent.toLowerCase.split(/\s+/);
 
-        if(cardContent.toLowerCase().indexOf(filter) > -1)
+        let match = false;
+        for (let j = 0; j < words.length; j++) {
+            if (words[j].indexOf(filter) > -1) {
+                match = true;
+                break;
+            }
+        }
+
+        if(match)
             card.style.display = "";
         else
-            card.style.display = "nenhum";
+            card.style.display = "none";
     }   
 }
